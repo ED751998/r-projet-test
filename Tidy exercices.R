1fd26252272b861194a81e824f8d364a2aebc182
Eric Desclos <eric.desclos1@gmail.com>
2024-12-09 14:18:02 +0100

    comit tidyexo q5

---
+++ Tidy exercices.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+
+tbl <- read_csv("data/piscines.csv")
+
+class(tbl)
+
+head(tbl, 5) # `head` est une fonction générique
+
+p1 <- filter(tbl, Longitude>153.02)
+
+p1$Longitude
+
+
+# Exercices ----
+
+## Iris ----
+data <- iris
+
+
+### Q1 ----
+Q1 <- data %>% 
+  select(Petal.Width,Species)
+
+### Q2 ----
+Q2 <- data %>% 
+  filter(Species %in% c("versicolor","virginica"))
+
+### Q3 ----
+Q3 <- data %>% 
+  filter(Species=="setosa") %>% 
+  summarise(n=n())
+
+print(Q3)
+
+#### Q4 ----
+
