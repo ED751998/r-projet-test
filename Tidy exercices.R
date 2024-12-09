e7016436fa581e35a388b76075fc0d335920add0
Eric Desclos <eric.desclos1@gmail.com>
2024-12-09 16:34:53 +0100

    final tidyverse 2

--- Tidy exercices.R
+++ Tidy exercices.R
@@ -110,3 +110,85 @@ Q10 <- hflights %>%
   group_by(UniqueCarrier) %>% 
   summarise("Retard" = mean(DepDelay,na.rm = T)) %>% 
   arrange(Retard)
+
+## Roland Garros ----
+
+### Q1 ----
+data_tennis <-read_csv("data/rolandgarros2013.csv") %>% as_tibble()
+
+### Q2 ----
+data_tennis %>% 
+  filter(Round==6) %>% 
+  select(Player1,Player2)
+
+### Q3 ----
+data_tennis %>% 
+  summarise(Aces=mean(ACE.1+ACE.2))
+
+### Q4 ----
+data_tennis %>% 
+  group_by(Round) %>% 
+  summarise(Aces=mean(ACE.1+ACE.2))
+
+### Q5 ----
+rg_joueurs <- data_tennis %>% 
+  select(Joueur=Player1) %>% 
+  rbind(data_tennis %>% 
+          select(Joueur=Player2)) %>% 
+  distinct()
+
+### Q6 ----
+Victoires <- data_tennis %>% 
+  group_by(Joueur=Player1) %>% 
+  summarise(Victoires=sum(Result)) %>% 
+  rbind(data_tennis %>% 
+          group_by(Joueur=Player2) %>% 
+          summarise(Victoires=sum(Result))) %>% 
+  group_by(Joueur) %>% 
+  summarise(Victoires_rg=sum(Victoires))
+
+rg_joueurs <- rg_joueurs %>% 
+  left_join(Victoires,by=c("Joueur"))
+
+### Q7 ----
+Australie <- read_csv("data/openaustralie2013.csv") %>% as_tibble()
+
+### Q8 ----
+tennis <- bind_rows(list(Australie %>% 
+                 mutate(Tournoi="OA",test="t"),data_tennis %>% 
+                 mutate(Tournoi="RG",test="j")),.id = "test")
+
+tennis <- Australie %>% mutate(Tournoi="OA") %>% 
+  rbind(data_tennis %>%  mutate(Tournoi="RG"))
+
+### Q9 ----
+Format_long <- tennis %>% 
+  group_by(Tournoi,Round) %>% 
+  summarise(Aces=mean(ACE.1+ACE.2))
+
+Format_large <- Format_long %>% 
+  pivot_wider(names_from = "Tournoi",values_from = "Aces")
+
+### Q10 ----
+oa_joueurs <- Australie %>% 
+  select(Joueur =Player1) %>% 
+  rbind(Australie %>% 
+          select(Joueur=Player2)) %>% 
+  distinct()
+
+Victoires_OA <- Australie %>% 
+  group_by(Joueur=Player1) %>% 
+  summarise(Victoires_oa=sum(Result)) %>% 
+  rbind(Australie %>% 
+          group_by(Joueur=Player2) %>% 
+          summarise(Victoires_oa=sum(Result))) %>% 
+  group_by(Joueur) %>% 
+  summarise(Victoires_oa=sum(Victoires_oa))
+
+oa_joueurs <- oa_joueurs %>% 
+  left_join(Victoires_OA,by=c("Joueur"))
+
+### Q11 ----
+
+# Jointure avec toutes les observations
+Final <- full_join(x = rg_joueurs,y = oa_joueurs,by=c("Joueur"))
